1ffffaa73ffd76cee93e4257aeec1b8f57c72b58
Juseong <hjs21847@ex-em.com>
2021-08-15 17:10:09 +0900

    3.0 작업전 기본 세팅

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,12 @@
 <template>
-  <main-header />
-  <main>
-    <router-view v-slot="{ Component, route }">
-      <keep-alive>
-        <component
-          :is="Component"
-          v-bind="route.params"
-        />
-      </keep-alive>
-    </router-view>
-  </main>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div>
+  <router-view/>
 </template>
 
-<script>
-import MainHeader from '@/components/common/Header';
-
-export default {
-  components: {
-    MainHeader,
-  },
-  setup() {
-  },
-};
-/* eslint-disable */
-</script>
-
 <style lang="scss">
-@font-face {
-  font-family: 'NanumSquareRound';
-  src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_two@1.0/NanumSquareRound.woff') format('woff');
-  font-weight: normal;
-  font-style: normal;
-}
-
-* {
-  padding: 0;
-  margin: 0;
-  border: 0;
-  user-select: none;
-  box-sizing: border-box;
-  font-family: 'NanumSquareRound', sans-serif;
-}
-
-body {
-  height: 100vh;
-  margin: 0;
-  background-color: #eee;
-}
-
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -56,29 +15,16 @@ body {
   color: #2c3e50;
 }
 
-main {
-  width: 1140px;
-  margin: 0 auto;
-  padding-top: 70px;
-}
+#nav {
+  padding: 30px;
 
-@media (max-width: 1025px) {
-  main {
-    width: 100%;
-  }
-}
+  a {
+    font-weight: bold;
+    color: #2c3e50;
 
-.contents {
-  padding: 50px 10px 30px;
-
-  > h2 {
-    text-align: left;
-    margin-bottom: 20px;
-  }
-
-  .contents-body {
-    padding: 30px 0;
-    text-align: left;
+    &.router-link-exact-active {
+      color: #42b983;
+    }
   }
 }
 </style>
